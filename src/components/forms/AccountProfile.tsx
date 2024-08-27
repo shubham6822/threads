408919f10e5f7a17ed9bec70c54b59e9cbfda14d
shubham6822 <sharmashubham6822@gmail.com>
2024-08-27 16:28:40 +0530

    start backend

--- src/components/forms/AccountProfile.tsx
+++ src/components/forms/AccountProfile.tsx
@@ -21,6 +21,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { useUploadThing } from "@/lib/uploadthing";
 import { isBase64Image } from "@/lib/utils";
 import { UserValidation } from "@/lib/validations/User";
+import { updateUser } from "@/actions/user.action";
 
 
 interface Props {
@@ -64,20 +65,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             }
         }
 
-        // await updateUser({
-        //     name: values.name,
-        //     path: pathname,
-        //     username: values.username,
-        //     userId: user.id,
-        //     bio: values.bio,
-        //     image: values.profile_photo,
-        // });
+        await updateUser({
+            name: values.name,
+            path: pathname,
+            username: values.username,
+            userId: user.id,
+            bio: values.bio,
+            image: values.profile_photo,
+        });
 
-        // if (pathname === "/profile/edit") {
-        //     router.back();
-        // } else {
-        //     router.push("/");
-        // }
+        if (pathname === "/profile/edit") {
+            router.back();
+        } else {
+            router.push("/");
+        }
     };
 
     const handleImage = (
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', "/api/webhook/clerk"])
 
 export default clerkMiddleware((auth, request) => {
     if (!isPublicRoute(request)) {
@@ -12,7 +12,5 @@ export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
     ],
 };
