d9c08febaba697674c1daab923d27ed28607787a
shubham6822 <sharmashubham6822@gmail.com>
2024-08-27 10:43:03 +0530

    clerk is done

---
+++ src/app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import { UserProfile } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+export default function OnBoarding() {
+    return (
+        <div>OnBoarding
+
+            <UserButton />
+        </div>
+    )
+}
